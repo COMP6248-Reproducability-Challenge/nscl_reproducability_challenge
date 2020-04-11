d0529f47bf0f4da9f173f3316564ec5b955bcada
thanakorn <thanakorn.panyapiang@outlook.com>
2020-04-11 16:39:54 +0100

    Custom DataLoader

--- nscl/datasets/clevr_dataset.py
+++ nscl/datasets/clevr_dataset.py
@@ -4,11 +4,13 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.dataloader import default_collate
 import numpy as np
 import json
-from question import Question
-from scene import Scene
+from nscl.datasets.question import Question
+from nscl.datasets.scene import Scene
 from PIL import Image
 import os.path as osp
 import torchvision.transforms as transforms
+
+__all__ = ['CLEVRDataset', 'build_clevr_dataset', 'build_clevr_dataloader']
 
 class CLEVRDataset(Dataset):
 
@@ -24,11 +26,7 @@ class CLEVRDataset(Dataset):
         question = Question(self.raw_questions[index])
         scene = Scene(self.raw_scenes[question.img_index])
         img = self.img_transform(Image.open(osp.join(self.img_location, question.img_file)).convert('RGB'))
-        sample = dict()
-        sample['img'] = img
-        sample['question'] = self.raw_questions[index]
-        # sample['scene'] = scene
-        return sample
+        return img, question, scene
 
     def __len__(self):
         return len(self.raw_questions)
@@ -39,3 +37,17 @@ def build_clevr_dataset(img_root, scenes_json, questions_json):
     ])
     dataset = CLEVRDataset(img_root, scenes_json, questions_json, img_transform,)
     return dataset
+
+def build_clevr_dataloader(dataset, batch_size, shuffle, drop_last):
+
+    def clevr_collate(batch):
+        img_batch = []
+        questions = []
+        scenes = []
+        for _batch in batch:
+            img_batch.append(_batch[0])
+            questions.append(_batch[1])
+            scenes.append(_batch[2])
+        return default_collate(img_batch), questions, scenes
+
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, collate_fn=clevr_collate)
