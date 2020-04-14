05a6ce79f6fcf3780dd003f8c4d2e6410434ce76
thanakorn <thanakorn.panyapiang@outlook.com>
2020-04-14 18:51:48 +0100

    Implement program translator

--- nscl/datasets/question.py
+++ nscl/datasets/question.py
@@ -13,16 +13,57 @@ class Question(object):
         self.split = json['split']
         self.answer = json['answer']
         self.question = json['question']
-        self.program = Question.get_program(json['program'])
+        self.program = Question.get_program_seq(json['program'])
 
     @staticmethod
-    def get_program(programs_json):
+    def get_program_seq(programs_json):
         return [Program(p) for p in programs_json]
 
 class Program(object):
     def __init__(self, json):
         super().__init__()
-        self.inputs = json['inputs']
-        self.function = json['function'] if 'function' in json else json['type']
-        self.values = json['value_inputs']
-        self.output = json['_output'] if '_output' in json else []
+        inputs = json['inputs']
+        function = json['function'] if 'function' in json else json['type']
+        value_inputs = json['value_inputs']
+
+        op = get_function_operation(function)
+        if op == 'scene':
+            self.operator = 'scene'
+        elif op.startswith('filter'):
+            self.operator = 'filter'
+            self.concept = value_inputs[0]
+        elif op.startswith('relate'):
+            self.operator = 'relate'
+            self.relational_concept = value_inputs[0]
+        elif op.startswith('same'):
+            self.operator = 'relate_attribute_equal'
+            self.attribute = get_function_attribute(function)
+        elif op in ('intersect', 'union'):
+            self.operator = op
+        elif op == 'unique':
+            pass  # We will ignore the unique operations.
+        else:
+            if op.startswith('query'):
+                self.operator = 'query'
+                self.attribute = get_function_attribute(function)
+            elif op.startswith('equal') and op != 'equal_integer':
+                self.operator = 'query_attribute_equal'
+                self.attribute = get_function_attribute(function)
+            elif op == 'exist':
+                self.operator = 'exist'
+            elif op == 'count':
+                self.operator = 'count'
+            elif op == 'equal_integer':
+                self.operator = 'count_equal'
+            elif op == 'less_than':
+                self.operator = 'count_less'
+            elif op == 'greater_than':
+                self.operator = 'count_greater'
+            else:
+                raise ValueError('Unknown CLEVR operation: {}.'.format(op))
+
+def get_function_operation(function):
+        return function.split('_')[0]
+    
+def get_function_attribute(function):
+    return function.split('_')[1]
