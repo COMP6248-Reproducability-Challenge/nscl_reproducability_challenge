e11d9c36f6ffd9c23bd673616ab7a0e3197be33a
thanakorn <thanakorn.panyapiang@outlook.com>
2020-04-29 11:04:03 +0100

    Notebook for testing attribute_embedding_space

--- nscl/models/embedding/attribute_embedding_space.py
+++ nscl/models/embedding/attribute_embedding_space.py
@@ -27,12 +27,14 @@ class AttributeEmbeddingSpace(nn.Module):
     def __init__(self, attribute_concept_map, input_dim, output_dim, margin=0.85, tau=0.25):
         super().__init__()
         self.all_attributes = attribute_concept_map.keys()
+        self.all_concepts = []
         self.attribute_operators = nn.Module()
         self.concept_embeddings = nn.Module()
         
         for (attr_id, attr) in enumerate(attribute_concept_map.keys()):
             self.attribute_operators.add_module(attr, AttributeOperator(input_dim, output_dim))
             for concept in attribute_concept_map[attr]:
+                self.all_concepts.append(concept)
                 self.concept_embeddings.add_module(concept, ConceptEmbedding(output_dim, len(self.all_attributes), attr_id))
 
         self.margin = margin
@@ -45,26 +47,25 @@ class AttributeEmbeddingSpace(nn.Module):
                         [.., .., .., ..], //obj_2 features
                         ...
                     ]
-        attribute : attribute of interest(color, material, size ...)
         concept : concept to filter(red, green, large ....)
 
-        return : 1D tensor representing the probability of each object is selected
+        return : 1D tensor representing the probability of each object has a given concept
     """
     def similarity(self, object_features: torch.Tensor, concept: str) -> torch.Tensor:
-        all_operators = [getattr(self.attribute_operators, attr) for attr in self.all_attributes]               # All attribute operators
+        all_operators = [getattr(self.attribute_operators, attr) for attr in self.all_attributes]
         object_embeddings = torch.stack([operator(object_features) for operator in all_operators], dim=-1)      # Map features to all spaces
-        object_embeddings = object_embeddings / object_embeddings.norm(p=2, dim=-1, keepdim=True)               # normalize object embeddings
+        object_embeddings = object_embeddings / object_embeddings.norm(p=2, dim=-1, keepdim=True)
         # print(object_embeddings.shape)
 
         concept_embedding = getattr(self.concept_embeddings, concept)
-        concept_vector = concept_embedding.concept_vector / concept_embedding.concept_vector.norm(p=2)          # reference vector(normalized)
+        concept_vector = concept_embedding.concept_vector / concept_embedding.concept_vector.norm(p=2)
         concept_vector = torch.stack([concept_vector for i in range(len(self.all_attributes))], dim=-1)         # Extend dimension so it can be broadcasted
         # print(concept_vector.shape)
         cosine_sim = ((object_embeddings * concept_vector).sum(dim=-2) - self.margin) / self.tau
         # print(cosine_sim.shape)
         
-        belong_vector = concept_embedding.belong_vector                                                         # belong vector
-        similarity = (belong_vector * cosine_sim).sum(dim=-1)                                                   # remove irrelevant attribute
+        belong_vector = concept_embedding.belong_vector
+        similarity = (belong_vector * cosine_sim).sum(dim=-1)                                                   # Remove irrelevant attribute
         # print(similarity.shape)
         # print(similarity)
         logits = F.sigmoid(similarity)
